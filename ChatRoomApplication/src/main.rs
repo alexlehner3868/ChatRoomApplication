01f8701cfb6eed4ed3d09f3d1dda637532e88f78
Alex <angrybird3868@gmail.com>
2025-10-10 11:12:20 -0400

    Created basic help command and quit command

--- ChatRoomApplication/src/main.rs
+++ ChatRoomApplication/src/main.rs
@@ -1,11 +1,35 @@
 use std::io::{self, Write};
 
-// Struct to represent the state of the current user
-struct CurrentUser {
-    username: String,
-    current_room: String,
+
+fn sign_up() -> bool {
+    // Alex TODO 
+    let mut signed_up = false;
+    // Get username. Check that it doesnt already exist. loop until valid username or quit
+    // Get password. Make sure password is vlaid. if not, reask for it 
+    // prohib usernames or passwords that start with \ 
+    signed_up
 }
 
+fn login() -> bool {
+    // Alex TODO
+    let mut login = false;
+    // looop in here. if invalid username or password, give the option to renter credentials, sign up or quit
+
+    login
+}
+
+
+fn print_help() {
+    // TODO --> Format help so it has all commands possible and seperates it by section (general(help, quit), intial( login, sign up), room manamgment etc)
+    println!("");
+    println!("[Help Menu]");
+    println!(r"/login                        Login with known username and passowrd");
+    println!(r"/sign_up                      Create a username and password");
+    println!(r"/quit                         Quit the application");
+    println!(r"/help                         See list of available commands");
+
+    println!()
+}
 
 fn alex_chat_room_loop(){
     println!("[Welcome to the Rust Chat Room Application!]");
@@ -13,7 +37,7 @@ fn alex_chat_room_loop(){
 
     while !logged_in {
         println!("");
-        println!(r"[Please /login or /sign_up]");
+        println!(r"[Please /login or /sign_up or get /help]");
         print!("> ");
         io::stdout().flush().unwrap();
         let mut user_input = String::new(); 
@@ -24,27 +48,20 @@ fn alex_chat_room_loop(){
                 
                 match user_input {
                     "/login" => {
-                        // TODO ALEX
+                        logged_in = login();
                     },
                     "/sign_up" => {
-                        // TODO ALEX
+                        logged_in = sign_up();
                     },
-                    "/help" => {
-                        println!("");
-                        println!("[Help Menu]");
-                        println!(r"/login                        Login with known username and passowrd");
-                        println!(r"/sign_up                      Create a username and password");
-                        println!(r"/quit                         Quit the application");
-                        println!(r"/help                         See list of available commands");
-
-                        println!()
+                    "/help" => { // Want to format help menu so that it has universal commands
+                        print_help()
                     },
                     "/quit" =>{
-                        println!("[Exiting Program]");
+                        println!("[Quitting Program]");
                         std::process::exit(1);
                     },
                     _ => {
-                        println!("[Unknown Command - Enter a valid command or use /help]]");
+                        println!("[Unknown Command");
                     }
                 }
             }
@@ -54,5 +71,5 @@ fn alex_chat_room_loop(){
 }
 
 fn main() {
-    //alex_chat_room_loop();
+    alex_chat_room_loop();
 }
