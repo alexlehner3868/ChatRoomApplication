faa331f0ec3666f8807d1c6252b5a518fa7dfb9f
Alex <angrybird3868@gmail.com>
2025-10-10 10:01:52 -0400

    Added Alex's todo list

--- ChatRoomApplication/src/main.rs
+++ ChatRoomApplication/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
+
+
+// Function to run the chatroom application for Alex to test
+// TODO: Move to Main eventually
+fn alex_chat_room_loop(){
+   println!("[Welcome to the Rust Chat Room Application!]");
+   
+}
+
 fn main() {
     println!("Hello, world!");
 }
