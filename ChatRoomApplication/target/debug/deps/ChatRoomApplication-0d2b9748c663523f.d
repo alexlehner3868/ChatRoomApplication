5249313131c6182b9793f12dc667ae96ab220cd5
Alex <angrybird3868@gmail.com>
2025-10-13 23:17:53 -0400

    Colored the terminal code

--- ChatRoomApplication/target/debug/deps/ChatRoomApplication-0d2b9748c663523f.d
+++
@@ -1,5 +0,0 @@
-/Users/alex/Documents/Programming/ChatRoomApplication/ChatRoomApplication/target/debug/deps/ChatRoomApplication-0d2b9748c663523f.d: src/main.rs
-
-/Users/alex/Documents/Programming/ChatRoomApplication/ChatRoomApplication/target/debug/deps/ChatRoomApplication-0d2b9748c663523f: src/main.rs
-
-src/main.rs:
--- ChatRoomApplication/target/debug/deps/ChatRoomApplication-b81808a3899889f1.d
+++
@@ -1,5 +0,0 @@
-/Users/alex/Documents/Programming/ChatRoomApplication/ChatRoomApplication/target/debug/deps/ChatRoomApplication-b81808a3899889f1.d: src/main.rs
-
-/Users/alex/Documents/Programming/ChatRoomApplication/ChatRoomApplication/target/debug/deps/ChatRoomApplication-b81808a3899889f1: src/main.rs
-
-src/main.rs:
