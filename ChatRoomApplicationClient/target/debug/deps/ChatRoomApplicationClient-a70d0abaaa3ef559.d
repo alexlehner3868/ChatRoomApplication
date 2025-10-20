c92833e633e49a2dc378f8aeafbed7e764aef104
Alex <angrybird3868@gmail.com>
2025-10-20 14:46:19 -0400

    remove run files

--- ChatRoomApplicationClient/target/debug/deps/ChatRoomApplicationClient-a70d0abaaa3ef559.d
+++
@@ -1,7 +0,0 @@
-/Users/alex/Documents/Programming/ChatRoomApplication/ChatRoomApplicationClient/target/debug/deps/ChatRoomApplicationClient-a70d0abaaa3ef559.d: src/main.rs src/color_formatting.rs src/chat_client.rs
-
-/Users/alex/Documents/Programming/ChatRoomApplication/ChatRoomApplicationClient/target/debug/deps/ChatRoomApplicationClient-a70d0abaaa3ef559: src/main.rs src/color_formatting.rs src/chat_client.rs
-
-src/main.rs:
-src/color_formatting.rs:
-src/chat_client.rs:
