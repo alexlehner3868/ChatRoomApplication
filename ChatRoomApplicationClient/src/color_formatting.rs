39d81d4a550f895e4e7a7690477af0bcc8297842
Alex <angrybird3868@gmail.com>
2025-11-12 22:06:40 -0500

    Comment color_formatting.rs

--- ChatRoomApplicationClient/src/color_formatting.rs
+++ ChatRoomApplicationClient/src/color_formatting.rs
@@ -2,6 +2,39 @@ use colored::*;
 use std::io::{self, Write};
 use chrono::{DateTime, Local};
 
+/*!
+ * color_formatting.rs
+ *
+ * This file has helper function to standardize formatting for client side printing
+ *
+ *  - header(text: &str):
+ *      Indicates the start of a section (eg Login or sign up)
+ *
+ *  - success(text: &str):
+ *      Used to confirm that a comment went through
+ *
+ *  - error(text: &str):
+ *      Used to report errors 
+ *
+ *  - warning(text: &str):
+ *      Used to draw attention to something that happenend
+ *
+ *  - info(text: &str):
+ *      Used for additonal info on system messages
+ *
+ *  - user_message(timestamp: &str, username: &str, message: &str):
+ *      Prints a chat room message that is recieved
+ *
+ *  - my_message(message: &str):
+ *      Prints a chat room message that you sent
+ *
+ *  - system_prompt(text: &str):
+ *      Prints the system prompt that indicates user input needed
+ *
+ *  - system_message(message: &str):
+ *      Prints a message from the system on a change in state (eg user joined a room)
+ */
+
 pub fn header(text: &str) {
     println!("");
     println!("{}", format!("[{}]", text).magenta().bold());
@@ -33,7 +66,7 @@ pub fn user_message(timestamp: &str, username: &str, message: &str) {
 }
 
 pub fn my_message(message: &str) {
-    let width = 80; // adjust to terminal width
+    let width = 80;
     println!("{:>width$} {}", "You:".blue().bold(), message.white(),width = width);
 }
 
